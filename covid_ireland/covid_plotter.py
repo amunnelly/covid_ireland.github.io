3a8d89117bfe6448b363965b9c9fa093c2383503
Anthony Munnelly <4349009+amunnelly@users.noreply.github.com>
2021-01-16 21:22:41 +0000

    fixed cumulative bug, improved xaxis formatting

--- covid_ireland/covid_plotter.py
+++ covid_ireland/covid_plotter.py
@@ -7,139 +7,161 @@ import json
 
 class CovidPlotter(object):
 
-	def __init__(self, metric, cumulative, dublin, title):
-		self.metric = metric
-		self.cumulative = cumulative
-		self.dublin = dublin
-		self.title = title
-		self.df = pd.read_csv('data/Covid19CountyStatisticsHPSCIreland.csv')
-		with open('data/counties.json') as f:
-			self.counties = json.load(f)
-		self.df['TimeStamp'] = pd.to_datetime(self.df['TimeStamp'])
-		self.find_case_density()
-		self.county_group = self.df.groupby('CountyName')
-		self.dailies = self.identify_daily_figures(self.county_group, 7)
-		self.daily_counties = self.dailies.groupby('CountyName')
-		
-		if cumulative:
-			self.create_plot(self.county_group)
-		else:
-			self.create_plot(self.daily_counties)
+    def __init__(self, metric, cumulative, dublin, title):
+        self.metric = metric
+        self.cumulative = cumulative
+        self.dublin = dublin
+        self.title = title
+        self.df = pd.read_csv('data/Covid19CountyStatisticsHPSCIreland.csv')
+        with open('data/counties.json') as f:
+            self.counties = json.load(f)
+        self.df['TimeStamp'] = pd.to_datetime(self.df['TimeStamp'])
+        self.find_case_density()
+        self.county_group = self.df.groupby('CountyName')
+        self.dailies = self.identify_daily_figures(self.county_group, 7)
+        self.daily_counties = self.dailies.groupby('CountyName')
+        
+        
+        if cumulative:
+            self.create_plot(self.county_group)
+        else:
+            self.create_plot(self.daily_counties)
 
-	def find_case_density(self):
-		self.df['Area'] = self.df['CountyName'].apply(lambda x: self.counties[x]['area'])
-		self.df['PopPerKmSquared'] = self.df['PopulationCensus16'] / self.df['Area']
-		self.df['CaseDensity'] = self.df['ConfirmedCovidCases'] / self.df['PopPerKmSquared']
-
-
-	def identify_daily_figures(self, counties, roll=0):
-		"""
-		
-		args:
-			counties (pd groupby object)
-		"""
-		holder = []
-		for a, b in counties:
-			temp = b.copy()
-			temp.sort_values('TimeStamp', inplace=True)
-			for i in range(1, len(temp)):
-				date_ = temp['TimeStamp'].iloc[i]
-				current = temp['ConfirmedCovidCases'].iloc[i]
-				prior = temp['ConfirmedCovidCases'].iloc[i-1]
-				cases = current - prior
-				holder.append([date_, a, cases])
-		dailies = pd.DataFrame(holder, columns=["TimeStamp", "CountyName", self.metric])
-		if roll > 0:
-			rolling = dailies[self.metric].rolling(roll).mean()
-			dailies[self.metric] = rolling
-		return dailies
+    def find_case_density(self):
+        self.df['Area'] = self.df['CountyName'].apply(lambda x: self.counties[x]['area'])
+        self.df['PopPerKmSquared'] = self.df['PopulationCensus16'] / self.df['Area']
+        self.df['CaseDensity'] = self.df['ConfirmedCovidCases'] / self.df['PopPerKmSquared']
 
 
-	def create_filename(self):
-		count_ = "_seven_day_average"
-		dublin = "_excluding_dublin"
-		if self.cumulative:
-			count_ = "_cumulative"
-		if self.dublin:
-			dublin = ""
-		filename = "".join([self.metric.lower(), count_, dublin])
-		return "".join(["plots/", filename, ".html"])
-		
+    def identify_daily_figures(self, counties, roll=0):
+        """
+        
+        args:
+            counties (pd groupby object)
+        """
+        holder = []
+        for a, b in counties:
+            temp = b.copy()
+            temp.sort_values('TimeStamp', inplace=True)
+            for i in range(1, len(temp)):
+                date_ = temp['TimeStamp'].iloc[i]
+                current = temp['ConfirmedCovidCases'].iloc[i]
+                prior = temp['ConfirmedCovidCases'].iloc[i-1]
+                cases = current - prior
+                holder.append([date_, a, cases])
+        dailies = pd.DataFrame(holder, columns=["TimeStamp", "CountyName", self.metric])
+
+        if roll > 0:
+            dailies = self.calculate_rolling_average(7, dailies)
+
+        return dailies
+
+    def calculate_rolling_average(self, roller, data):
+
+        holder = []
+        counties = data.groupby('CountyName')
+        
+        for a, b in counties:
+            temp = b.copy()
+            temp[self.metric] = temp[self.metric].rolling(roller).mean()
+            holder.append(temp)
+        
+        return pd.concat(holder)
+            
 
 
-	def create_plot(self, counties):
-		filename = self.create_filename()
-		bp.output_file(filename, title=self.title)
-		p = bp.figure(title=self.title,
-			x_axis_type="datetime",
-			width=1200,
-			height=700)
-		legend_items = []
-		for a, b in counties:
-			if self.dublin==False and a =="Dublin":
-				continue
-			temp = b.copy()
-			temp['StrDate'] = temp['TimeStamp'].apply(lambda x: x.strftime("%a, %b %d"))
-			source = bp.ColumnDataSource(temp)
-			leg = p.line("TimeStamp",
-				self.metric,
-				source=source,
-				line_width=2,
-				line_color=self.counties[a]['color'],
-				line_dash=self.counties[a]['dash'])
-			legend_items.append((a, [leg]))
+    def create_filename(self):
+        count_ = "_seven_day_average"
+        dublin = "_excluding_dublin"
+        if self.cumulative:
+            count_ = "_cumulative"
+        if self.dublin:
+            dublin = ""
+        filename = "".join([self.metric.lower(), count_, dublin])
+        return "".join(["plots/", filename, ".html"])
+        
 
 
-		legend = bm.Legend(items=legend_items,
-					location='top_right',
-					orientation='vertical',
-					border_line_color="black")
+    def create_plot(self, counties):
+        filename = self.create_filename()
+        bp.output_file(filename, title=self.title)
+        p = bp.figure(title=self.title,
+            x_axis_type="datetime",
+            width=1200,
+            height=700)
+        legend_items = []
+        for a, b in counties:
+            if self.dublin==False and a =="Dublin":
+                continue
+            temp = b.copy()
+            temp['StrDate'] = temp['TimeStamp'].apply(lambda x: x.strftime("%a, %b %d"))
+            source = bp.ColumnDataSource(temp)
+            leg = p.line("TimeStamp",
+                self.metric,
+                source=source,
+                line_width=2,
+                line_color=self.counties[a]['color'],
+                line_dash=self.counties[a]['dash'])
+            legend_items.append((a, [leg]))
 
-		p.add_layout(legend, 'right')
 
-		p.legend.click_policy="hide"
-		p.legend.label_text_font_size = '12px'
-		p.legend.label_text_font = 'Helvetica'
-		p.legend.location = "top_left"
-		p.legend.background_fill_color = 'slategray'
-		p.legend.background_fill_alpha = 0.5
+        legend = bm.Legend(items=legend_items,
+                    location='top_right',
+                    orientation='vertical',
+                    border_line_color="black")
 
-		tools = bm.HoverTool(tooltips=[("Date","@StrDate"),
-										("County","@CountyName"),
-										("Cases","@ConfirmedCovidCases")])
-		p.add_tools(tools)
+        p.add_layout(legend, 'right')
 
-		p.title.text_font = "Helvetica"
-		p.title.text_font_size = "18px"
+        p.legend.click_policy="hide"
+        p.legend.label_text_font_size = '12px'
+        p.legend.label_text_font = 'Helvetica'
+        p.legend.location = "top_left"
+        p.legend.background_fill_color = 'slategray'
+        p.legend.background_fill_alpha = 0.5
 
-		p.background_fill_color = 'slategray'
-		p.background_fill_alpha = 0.5
+        tools = bm.HoverTool(tooltips=[("Date","@StrDate"),
+                                        ("County","@CountyName"),
+                                        ("Cases","@ConfirmedCovidCases{0, 0}")])
+        p.add_tools(tools)
 
-		bp.show(p)
+        p.title.text_font = "Helvetica"
+        p.title.text_font_size = "18px"
+
+        p.background_fill_color = 'slategray'
+        p.background_fill_alpha = 0.5
+        
+        p.xaxis.formatter = bm.DatetimeTickFormatter(months=["%b, %Y"], days=["%b, %d"], hours=["%H:%M"])
+
+
+        bp.show(p)
 
 if __name__ == "__main__":
-	x = CovidPlotter('ConfirmedCovidCases',
-		True,
-		True,
-		"Confirmed Cases by County, Cumulative")
-	x = CovidPlotter('ConfirmedCovidCases',
-		True,
-		False,
-		"Confirmed Cases by County, Cumulative, Excluding Dublin")
-	x = CovidPlotter('ConfirmedCovidCases',
-		False,
-		True,
-		"Confirmed Cases by County, Seven-Day-Average")
-	x = CovidPlotter('ConfirmedCovidCases',
-		False,
-		False,
-		"Confirmed Cases by County, Seven-Day-Average, Excluding Dublin")
-	x = CovidPlotter('PopulationProportionCovidCases',
-		True,
-		True,
-		"Population Proportion per 100,000, Cumulative")
-	x = CovidPlotter('CaseDensity',
-		True,
-		True,
-		"Case Density per County per Square Kilometre, Cumulative")
-	x.df.to_csv('data/munged/df.csv')
+    x = CovidPlotter('ConfirmedCovidCases',
+        True,
+        True,
+        "Confirmed Cases by County, Cumulative")
+    x = CovidPlotter('ConfirmedCovidCases',
+        True,
+        False,
+        "Confirmed Cases by County, Cumulative, Excluding Dublin")
+    x = CovidPlotter('ConfirmedCovidCases',
+        False,
+        True,
+        "Confirmed Cases by County, Seven-Day-Average")
+    x = CovidPlotter('ConfirmedCovidCases',
+        False,
+        False,
+        "Confirmed Cases by County, Seven-Day-Average, Excluding Dublin")
+    x = CovidPlotter('PopulationProportionCovidCases',
+        True,
+        True,
+        "Population Proportion per 100,000, Cumulative")
+    # x = CovidPlotter('CaseDensity',
+    #     True,
+    #     True,
+    #     "Case Density per County per Square Kilometre, Cumulative")
+    # x = CovidPlotter('CaseDensity',
+    #     False,
+    #     True,
+    #     "Case Density per County per Square Kilometre, Seven-Day-Average")
+    x.df.to_csv('data/munged/df.csv')
